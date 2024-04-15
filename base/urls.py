1404cbdb781b358bb22d557928495231630f7e35
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-14 19:12:41 -0600

    added urls for profile, delete message, update user, topics and activity in mobile version

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, room, createRoom, updateRoom, deleteRoom, loginPage, logoutUser, registerUser
+from .views import home, room, createRoom, updateRoom, deleteRoom, loginPage, logoutUser, registerUser, deleteMessage, userProfile, updateUser, topicPage, activityPage
 
 urlpatterns = [
     path('login/', loginPage, name='login'),
@@ -7,7 +7,12 @@ urlpatterns = [
     path('logout/', logoutUser, name='logout'),
     path('', home, name='home'),
     path('room/<str:pk>/', room, name='room'),
+    path('profile/<str:pk>/', userProfile, name='user-profile'),
     path('create-room', createRoom, name='create-room'),
     path('update-room/<str:pk>/', updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>/', deleteMessage, name='delete-message'),
+    path('update-user/', updateUser, name='update-user'),
+    path('topics/', topicPage, name='topics'),
+    path('activity/', activityPage, name='activity'),
 ]
