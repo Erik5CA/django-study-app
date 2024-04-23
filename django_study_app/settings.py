5301093d63c2310e41c82ee92ad5958d6cc1e6f5
Erik5CA <erik.castillo.anzures@gmail.com>
2024-04-23 15:47:06 -0600

    updated settings

--- django_study_app/settings.py
+++ django_study_app/settings.py
@@ -98,6 +98,10 @@ DATABASES = {
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
